54f20063fac2709917c34295ac35bc139ffc4eba
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2019-10-22 00:50:56 +0000

    Updated version

--- wme-ukrkadastr-layer.js
+++ wme-ukrkadastr-layer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           WME Ukrkadastr Layer
 // @author         Andrei Pavlenko
-// @version        0.3.2
+// @version        0.4.0
 // @include        /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
 // @exclude        https://www.waze.com/user/*editor/*
 // @exclude        https://www.waze.com/*/user/*editor/*
